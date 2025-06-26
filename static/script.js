9771358f6c301e2bb499b16114913babc9451ffc
jhansi <jhansijamuna23@gamail.com>
2025-06-26 20:45:24 +0530

    first commit

---
+++ static/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const menuList = document.getElementById('menu-list');
+    menuList.classList.toggle('show');
+  }
+  
+  function showMessage() {
+  alert("Thank you for your interest! Let's connect on LinkedIn.");
+}
